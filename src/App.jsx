c28958e8f934da1320a4c6c115c6b9429c95445e
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-26 00:57:34 +0530

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
+import NavBar from './components/NavBar';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <NavBar />
+    </main>
   )
 }
 
