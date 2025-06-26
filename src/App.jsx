728f02011a3d44ce98574a6bd285735b56dcbbc1
Vidura Priyadarshana <vidura200427@gmail.com>
2025-06-26 10:54:58 +0530

    hero-component-update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
 import NavBar from './components/NavBar';
+import Hero from './components/Hero';
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -9,6 +10,8 @@ const App = () => {
   return (
     <main>
       <NavBar />
+      <Hero />
+      <div className='h-dvh bg-black'></div>
     </main>
   )
 }
